4a0c83be024a24e9a450a665402748c645bdfc6e
AspiringSaint <wilberttosoydev@outlook.com>
2025-09-15 01:10:29 +0800

    initial landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom"
 import { Toaster } from 'react-hot-toast';
 
+import LandingPage from "./pages/LandingPage";
+
 const App = () => {
   return (
     <>
       <Toaster position="top-right" reverseOrder={false} />
       
       <Routes>
-        
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </>
   )
